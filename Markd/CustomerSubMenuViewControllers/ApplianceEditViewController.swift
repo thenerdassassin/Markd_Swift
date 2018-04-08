7fe079856d779cd402ac494d01a6f24336b5fa96
Joshua Schmidt <schmidt.uconn@gmail.com>
2018-04-08 15:18:17 -0400

    Plumber Edit button leads to ApplianceEdit page

---
+++ Markd/CustomerSubMenuViewControllers/ApplianceEditViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ApplianceEditViewController.swift
+//  Markd
+//
+//  Created by Joshua Schmidt on 4/8/18.
+//  Copyright © 2018 Joshua Daniel Schmidt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ApplianceEditViewController: UIViewController {
+    
+}
