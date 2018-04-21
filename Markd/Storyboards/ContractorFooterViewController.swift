f170c551a442192bcfbd50b9ed6e466770d43792
Joshua Schmidt <schmidt.uconn@gmail.com>
2018-04-21 13:25:49 -0400

    Added KingFisher

--- Markd/Storyboards/ContractorFooterViewController.swift
+++ Markd/Storyboards/ContractorFooterViewController.swift
@@ -8,15 +8,13 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 public class ContractorFooterViewController: UIViewController, OnGetContractorListener {
-    
     @IBOutlet weak var companyLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UIButton!
     @IBOutlet weak var websiteLabel: UIButton!
     @IBOutlet weak var companyLogoImageView: UIImageView!
-    
-    var TODO_SetImageToLogo_😱:AnyObject?
     
     private func configureView(with contractor: Contractor) {
         print(contractor)
@@ -44,6 +42,13 @@ public class ContractorFooterViewController: UIViewController, OnGetContractorLi
                     phoneNumberLabel.isUserInteractionEnabled = false
                 }
             }
+        }
+        if let companyLogo = companyLogoImageView, let file = contractor.getContractorDetails()?.getWebsiteUrl() {
+            print(file)
+            let placeholderImage = UIImage(named: "ic_action_pages")!
+            let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/markd-schmidt-happens.appspot.com/o/images%2Flogos%2Fs5VWMQvH17ZJnVqxtOkqvWpufmu2%2Fb79a0ef6-4d7d-41fe-b10d-e87c22eaaa43?alt=media&token=a1339f28-5da1-43b7-99f8-6018ab2cf6dd")
+            companyLogo.kf.indicatorType = .activity
+            companyLogo.kf.setImage(with:url, placeholder:placeholderImage)
         }
     }
     
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# platform :ios, '9.3'
 
 target 'Markd' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -11,6 +11,7 @@ target 'Markd' do
   pod 'Firebase/Auth'
   pod 'Firebase/Storage'
   pod 'Firebase/Messaging'
+  pod 'Kingfisher', '~> 4.0'
 
 workspace 'Mard.xcworkspace'
 
