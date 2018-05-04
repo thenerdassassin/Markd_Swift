24fb455427fcf46f4e50f0267ac8b51c7843132e
Joshua Schmidt <schmidt.uconn@gmail.com>
2018-05-04 16:29:17 -0400

    Added Sections to ContractorService TableView

--- Markd/CustomerSubMenuViewControllers/ContractorServiceTableViewController.swift
+++ Markd/CustomerSubMenuViewControllers/ContractorServiceTableViewController.swift
@@ -9,36 +9,76 @@
 import UIKit
 
 class ContractorServiceTableViewController: UITableViewController {
-
+    private var contractor:String?
+    private var comments:String?
+    private var installDate:String?
+    private var files:[AnyObject]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let addButton = UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = addButton
+        tableView.tableFooterView = UIView()
     }
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if(section == 0) {
+            return 3
+        } else if(section == 1) {
+            guard let files = files else {
+                return 0
+            }
+            return files.count
+        }
         return 0
     }
+    override func tableView(_ tableView : UITableView,  titleForHeaderInSection section: Int) -> String {
+        if section == 0  {
+            return "Service Details"
+        } else if section == 1 {
+            return "Files"
+        }
+        return ""
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 50.0
+        } else {
+            return 30.0
+        }
+    }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        if(indexPath.section == 0) {
+            switch indexPath.row {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "contractorTableCell", for: indexPath)
+                // Configure the cell...
+                return cell
+            case 1:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "commentsCell", for: indexPath)
+                // Configure the cell...
+                return cell
+            case 2:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+                // Configure the cell...
+                return cell
+            default:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "serviceFileTableCell", for: indexPath)
+                // Configure the cell...
+                return cell
+            }
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "serviceFileTableCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
