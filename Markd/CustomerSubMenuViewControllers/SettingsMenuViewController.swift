21850b68edfbc5efa2840b78a32258b2d3e6a3a9
Joshua Schmidt <schmidt.uconn@gmail.com>
2018-07-28 15:46:12 -0400

    Added sign out row to SettingsMenu

--- Markd/CustomerSubMenuViewControllers/SettingsMenuViewController.swift
+++ Markd/CustomerSubMenuViewControllers/SettingsMenuViewController.swift
@@ -26,7 +26,7 @@ class SettingsMenuViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         var TODO_AddRows:AnyObject? //Edit Profile, Edit Home, Find Contractor, Reset Password, Help?
-        return 1
+        return 2
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
@@ -34,7 +34,20 @@ class SettingsMenuViewController: UITableViewController {
             cell.textLabel?.text = "Find Contractors"
             cell.detailTextLabel?.text = "Set your personal contractors."
             return cell
+        } else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = "Sign Out"
+            cell.detailTextLabel?.text = "Log out of the current account."
+            return cell
         }
         return UITableViewCell()
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            performSegue(withIdentifier: "findContractorSegue", sender: self)
+        } else if indexPath.row == 1 {
+            print("Signing out")
+        }
     }
 }
