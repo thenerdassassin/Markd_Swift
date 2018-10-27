f024a6ac1cf545407b3ba46031fd593a1e013231
Joshua Schmidt <schmidt.uconn@gmail.com>
2018-10-27 11:45:29 -0400

    SettingsViewController shows options based on userType

--- Markd/CustomerMenuViewControllers/SettingsMenuViewController.swift
+++ Markd/CustomerMenuViewControllers/SettingsMenuViewController.swift
@@ -7,20 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsMenuViewController: UITableViewController {
-    let options = [("Find Contractors", "Set your personal contractors.", "findContractorSegue"),
+    private let authentication = FirebaseAuthentication.sharedInstance
+    var options:[(String, String, String)] = []
+    let customerOptions = [("Find Contractors", "Set your personal contractors.", "findContractorSegue"),
                     ("Edit Home", "Change address, bedrooms, square footage, etc.", "editHomeSegue"),
                     ("Edit Profile", "Change email or name on account.", "editProfileSegue"),
                     ("Contact Us", "Ask for help or tell us what you would like added.", "helpSegue"),
-                    ("Reset Password", "An email will be sent to change password.", nil),
-                    ("Sign Out", "Log out of this account.", nil)
-                  ]
+                    ("Reset Password", "An email will be sent to change password.", "resetPasswordSegue"),
+                    ("Sign Out", "Log out of this account.", "signOutSegue")]
+    let contractorOptions = [("Edit Profile", "Change email or name on account.", ""),
+                             ("Edit Company", "Change company telephone, phone number, etc.", "editCompanySegue"),
+                             ("Contact Us", "Ask for help or tell us what you would like added.", "helpSegue"),
+                             ("Reset Password", "An email will be sent to change password.", ""),
+                             ("Sign Out", "Log out of this account.", "signOutSegue")]
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if authentication.checkLogin(self) {
+            authentication.getUserType(in: self, listener: setUpOptions)
+        } else {
+            performSegue(withIdentifier: "unwindToLoginSegue", sender: self)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "backgroundTexture")!)
         tableView.tableFooterView = UIView()
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
         ViewControllerUtilities.insertMarkdLogo(into: self)
         tableView.reloadData()
     }
@@ -43,17 +57,38 @@ class SettingsMenuViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        guard let segue = options[indexPath.row].2 else {
-            if indexPath.row == options.count-1 {
-                print("Signing out")
-                FirebaseAuthentication.sharedInstance.signOut(self)
-                return
-            } else {
-                print("Send password reset email")
-                FirebaseAuthentication.sharedInstance.forgotPassword(self, withEmail: FirebaseAuthentication.sharedInstance.getCurrentUser()!.email!)
-                return
-            }
+        let segue = options[indexPath.row].2
+        if segue == "signOutSegue" {
+            print("Signing out")
+            FirebaseAuthentication.sharedInstance.signOut(self)
+        } else if segue == "resetPasswordSegue" {
+            print("Send password reset email")
+            FirebaseAuthentication.sharedInstance.forgotPassword(self, withEmail: FirebaseAuthentication.sharedInstance.getCurrentUser()!.email!)
+        } else {
+            performSegue(withIdentifier: segue, sender: self)
         }
-        performSegue(withIdentifier: segue, sender: self)
+    }
+    
+    // Mark:- Helper
+    private func setUpOptions(snapShot: DataSnapshot) {
+        print("Setting up options")
+        guard let snapShotDictionary = snapShot.value as? Dictionary<String, AnyObject> else {
+            print("Error")
+            authentication.signOut(self)
+            AlertControllerUtilities.somethingWentWrong(with: self, because: MarkdError.UnexpectedNil)
+            return
+        }
+        let userType = snapShotDictionary["userType"] as? String
+        if(userType == "customer") {
+            options = customerOptions
+            tableView.reloadData()
+        } else if (userType == "contractor") {
+            options = contractorOptions
+            tableView.reloadData()
+        } else {
+            authentication.signOut(self)
+            print(userType!)
+            AlertControllerUtilities.somethingWentWrong(with: self, because: MarkdError.UnsupportedConfiguration)
+        }
     }
 }
