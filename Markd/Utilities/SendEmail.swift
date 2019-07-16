055b3175c7e2597c7b21e7eba4a7c34d00150374
Joshua Schmidt <schmidt.uconn@gmail.com>
2019-07-15 21:38:52 -0700

    Fixed email support handler

--- Markd/Utilities/SendEmail.swift
+++ Markd/Utilities/SendEmail.swift
@@ -7,48 +7,41 @@
 //
 
 import Foundation
+import Firebase
 
 public struct SendEmail {
-    private static let TO:String = "schmidt.uconn@gmail.com"
-    private static let DOMAIN:String = "sandbox11b0c4f4831d4edaa9d23ba61c6d125f.mailgun.org"
-    private static let API_KEY:String = "key-98ae211f5d8dd12c4639ab41ef6ccf9a"
-    enum MyError: Error {
-        case GetUrlFailure(String)
-    }
-    
-    private static func getUrl(_ message: String, _ from: String) -> URL? {
-        let urlComponents = NSURLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.mailgun.net"
-        urlComponents.path = "/v3/\(DOMAIN)/messages"
-        let to = URLQueryItem(name: "to", value: TO)
-        let text = URLQueryItem(name: "text", value: message)
-        let from = URLQueryItem(name: "from", value: from)
-        let subject = URLQueryItem(name: "subject", value: "Markd help")
-        urlComponents.queryItems = [to, from, subject, text]
-        return urlComponents.url
-    }
-    
-    public static func send(_ message: String, from:String, successHandler: @escaping (Data?, URLResponse?) -> Void, errorHandler: @escaping (Error) -> Void) {
-        let loginData = String(format: "%@:%@", "api", API_KEY).data(using: String.Encoding.utf8)!
-        let base64LoginData = loginData.base64EncodedString()
-        let session = URLSession(configuration: URLSessionConfiguration.default)
+    private static let database:DatabaseReference = Database.database().reference().child("support");
+    private struct EmailMessage {
+        let from:String
+        let message:String
+        let date:String
         
-        if let url = getUrl(message, from) {
-            debugPrint(url)
-            let request:NSMutableURLRequest = NSMutableURLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
-            let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
-                if let error = error  {
-                    errorHandler(error)
-                } else {
-                    successHandler(data, response)
-                }
+        init(from email:String, message: String, date:String) {
+            self.from = email
+            self.message = message
+            self.date = date
+        }
+        
+        public func toDictionary() -> Dictionary<String, AnyObject> {
+            var dictionary = Dictionary<String, AnyObject>()
+            dictionary["from"] = self.from as AnyObject
+            dictionary["message"] = self.message as AnyObject
+            dictionary["date"] = self.date as AnyObject
+            return dictionary
+        }
+    }
+    
+    public static func send(_ message: String, from email:String, successHandler: @escaping () -> Void, errorHandler: @escaping (Error) -> Void) {
+        database.child(StringUtilities.getCurrentDateString(seperatedBy: "/") + "/" + UUID().uuidString).setValue(
+            EmailMessage(from: email, message:message, date:StringUtilities.getCurrentDateString()).toDictionary()
+        ) { (error:Error?, ref:DatabaseReference) in
+            if let error = error {
+                print("Data could not be saved: \(error).")
+                errorHandler(error)
+            } else {
+                print("Data saved successfully!")
+                successHandler()
             }
-            task.resume()
-        } else {
-           errorHandler(MyError.GetUrlFailure("message:\(message), from:\(from)"))
         }
     }
 }
